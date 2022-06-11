90f376e77fcfd5e1e4eba5c23f912a234f4c7aa4
Aurimas Ž <82368117+Aurimas-stack@users.noreply.github.com>
2022-06-11 12:15:22 +0300

    floats

--- 4 week/2 task/app.js
+++ 4 week/2 task/app.js
@@ -1,15 +1,15 @@
 const add = (number_1, number_2) => {
-    return number_1 + number_2
-}
+  return number_1 + number_2;
+};
 const subtract = (number_1, number_2) => {
-    return number_1 - number_2
-}
+  return number_1 - number_2;
+};
 const multiply = (number_1, number_2) => {
-    return number_1 * number_2
-}
+  return number_1 * number_2;
+};
 const divide = (number_1, number_2) => {
-    return number_1 / number_2
-}
+  return number_1 / number_2;
+};
 
 const calculationArray = [];
 const displayArray = [];
@@ -17,107 +17,121 @@ const currentPressedBtnArray = [];
 let currentPressedBtn = null;
 
 const operate = (numb1, numb2, operator) => {
-    const number_1 = +numb1;
-    const number_2 = +numb2;
-    const patternInteger = /^-?[0-9]+$/
-    let calculationResult;
+  const number_1 = +numb1;
+  const number_2 = +numb2;
+  const patternInteger = /^-?[0-9]+$/;
+  let calculationResult;
 
+  if (operator === "+") {
+    calculationResult = add(number_1, number_2);
+  }
+  if (operator === "-") {
+    calculationResult = subtract(number_1, number_2);
+  }
+  if (operator === "X") {
+    calculationResult = multiply(number_1, number_2);
+  }
 
-    if(operator === "+") {
-        calculationResult = add(number_1, number_2)
-    }
-    if(operator === "-") {
-        calculationResult = subtract(number_1, number_2)
-    }
-    if(operator === "X") {
-        calculationResult = multiply(number_1, number_2)
-    }
+  if (operator === "/") {
+    calculationResult = divide(number_1, number_2);
+  }
 
-    if(operator === "/") {
-        calculationResult = divide(number_1, number_2)
-    }
+  if (!patternInteger.test(calculationResult)) {
+    calculationResult = calculationResult.toFixed(2);
+  }
 
-    if(!patternInteger.test(calculationResult)) {
-        calculationResult = calculationResult.toFixed(3)
+  calculationArray[0] = calculationResult;
+};
 
-    }
+const calculate = () => {
+  if (calculationArray.length > 0) {
+    calculationArray.push(currentPressedBtnArray.join(""));
+    currentPressedBtnArray.length = 0;
+  }
 
-    calculationArray[0] = calculationResult
+  if (!calculationArray[0] || !calculationArray[2] || !calculationArray[1]) {
+    return;
+  }
 
-}
-
-
-const calculate = (action) => {;
-    const operators = ["X", "/", "X", "-", "+"]
-    currentPressedBtn = action;
-
-    if(currentPressedBtn === "AC") {
-        calculationArray.length = 0;
-        currentPressedBtnArray.length = 0;
-        displayArray.length = 0;
-        return;
-    }
-
-    if(operators.includes(currentPressedBtn) && currentPressedBtnArray.length >= 1) {
-        calculationArray.push(currentPressedBtnArray.join(""))
-        currentPressedBtnArray.length = 0;
+  while (calculationArray.length > 1) {
+    if (+calculationArray[2] === 0 && calculationArray[1] === "/") {
+      displayArray.length = 0;
+      displayArray[0] = "You can't divide by zero!";
+      return;
     }
 
-    if(!isNaN(currentPressedBtn) && operators.includes(currentPressedBtnArray[0])) {
-        calculationArray.push(currentPressedBtnArray[0])
-        currentPressedBtnArray.length = 0;
-    }
+    operate(calculationArray[0], calculationArray[2], calculationArray[1]);
+    calculationArray.splice(1, 2);
+  }
+};
 
-    if(!isNaN(currentPressedBtn)) {
-        currentPressedBtnArray.push(currentPressedBtn)
-        displayArray.push(currentPressedBtn)
-    }
+const calculator = (action) => {
+  const operators = ["X", "/", "X", "-", "+"];
+  currentPressedBtn = action;
 
-    if(operators.includes(currentPressedBtn)) {
-        currentPressedBtnArray[0] = currentPressedBtn
-        displayArray.push(currentPressedBtn)
-    }
+  if (currentPressedBtn === "AC") {
+    calculationArray.length = 0;
+    currentPressedBtnArray.length = 0;
+    displayArray.length = 0;
+    return;
+  }
 
-    if(currentPressedBtn === ".") {
+  if (
+    operators.includes(currentPressedBtn) &&
+    currentPressedBtnArray.length >= 1 &&
+    !operators.includes(calculationArray[calculationArray.length - 1])
+  ) {
+    calculationArray.push(currentPressedBtnArray.join(""));
+    currentPressedBtnArray.length = 0;
+  }
 
-    }
+  if (
+    !isNaN(currentPressedBtn) &&
+    operators.includes(currentPressedBtnArray[0]) &&
+    !operators.includes(calculationArray[calculationArray.length - 1])
+  ) {
+    calculationArray.push(currentPressedBtnArray[0]);
+    currentPressedBtnArray.length = 0;
+  }
 
+  if (!isNaN(currentPressedBtn)) {
+    currentPressedBtnArray.push(currentPressedBtn);
+    displayArray.push(currentPressedBtn);
+  }
 
-    if(currentPressedBtn === "=") {
+  if (operators.includes(currentPressedBtn)) {
+    currentPressedBtnArray[0] = currentPressedBtn;
+  }
 
-        if(calculationArray.length > 0) {
-            calculationArray.push(currentPressedBtnArray.join(""))
-            currentPressedBtnArray.length = 0
-        }
+  if (
+    operators.includes(currentPressedBtn) &&
+    !operators.includes(displayArray[displayArray.length - 1])
+  ) {
+    displayArray.push(currentPressedBtn);
+  }
 
-        if(!calculationArray[0] || !calculationArray[2] || !calculationArray[1]) {
-            return;
-        }
+  if (
+    currentPressedBtn === "." &&
+    !isNaN(currentPressedBtnArray.length - 1) &&
+    !currentPressedBtnArray.includes(".")
+  ) {
+    currentPressedBtnArray.push(currentPressedBtn);
+    displayArray.push(currentPressedBtn);
+  }
 
-        while(calculationArray.length > 1) {
-            if(+calculationArray[2] === 0 && calculationArray[1] === "/") {
-                displayArray.length = 0;
-                displayArray[0] = "You can't divide by zero!"
-                return;
-            }
-
-
-            operate(calculationArray[0], calculationArray[2], calculationArray[1])
-            calculationArray.splice(1,2);
-
-        }
-        displayArray.length = 0;
-        displayArray[0] = calculationArray[0]
-    }
-
+  if (currentPressedBtn === "=") {
+    calculate();
+    displayArray.length = 0;
+    displayArray[0] = calculationArray[0];
+  }
 };
 
 const btns = document.querySelectorAll(".buttons")[0].children;
 const calculationScreen = document.getElementById("calculation_screen");
 
-for(let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", () => {
-        calculate(btns[i].outerText)
-        calculationScreen.textContent = displayArray.join("");
-    })
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", () => {
+    calculator(btns[i].outerText);
+    calculationScreen.textContent = displayArray.join("");
+  });
 }
